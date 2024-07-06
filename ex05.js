0b8bca18bce0b23b32ccc5d06e90317ac13af68f
z4nt <antonielf1203@gmail.com>
2024-07-06 13:45:04 -0300

    aperfeiçoamento

--- ex05.js
+++ ex05.js
@@ -1,10 +1,10 @@
 var prato = [0, 180, 230, 250, 350]
 var sobremesa = [0, 75, 110, 170, 200]
 var bebida = [0, 20, 70, 100, 65]
-pratoInput = 1
-sobremesaInput = 2
-bebidaInput = 3
-pegarPrato(1,2,3)
+pratoInput = prompt('Digite a opção de prato que deseja: ')
+sobremesaInput = prompt('Digite a opção de sobremesa que deseja: ')
+bebidaInput = prompt('Digite a opção de bebida que deseja: ')
+pegarPrato(pratoInput,sobremesaInput,bebidaInput)
 function pegarPrato(pratoInput,sobremesaInput,bebidaInput) {
     if (parseInt(pratoInput) < 1 || parseInt(pratoInput) > 4) {
         console.log('Digite uma opção válida')
